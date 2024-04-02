5a97a9b7f254efefbc2580246267d787801bdc44
nileshnawale24 <nileshnawale24>
2024-04-02 13:30:49 +0000

    created_ingesting_all_files_notebook

--- bronze/ingest_all_files.py
+++ bronze/ingest_all_files.py
@@ -1,7 +1,31 @@
 # Databricks notebook source
-# MAGIC %run ../bronze
+dbutils.notebook.run('../bronze/ingest_circuits_file',0,{'file_name':'circuits','container_name':'silver'})
+
 
 # COMMAND ----------
 
-dbutils.notebook.run('../bronze/ingest_circuits_file',0,{'file name':''})
+dbutils.notebook.run('../bronze/ingest_constructors_file',0,{'file_name':'constructors','container_name':'silver'})
 
+# COMMAND ----------
+
+dbutils.notebook.run('../bronze/ingest_drivers_file',0,{'file_name':'drivers','container_name':'silver'})
+
+# COMMAND ----------
+
+dbutils.notebook.run('../bronze/ingest_multiple_lap_times_file',0,{'folder_name':'lap_times','container_name':'silver'})
+
+# COMMAND ----------
+
+dbutils.notebook.run('../bronze/ingest_multiple_qualifying_file',0,{'folder_name':'qualifying','container_name':'silver'})
+
+# COMMAND ----------
+
+dbutils.notebook.run('../bronze/ingest_pit_stops_file',0,{'file_name':'pit_stops','container_name':'silver'})
+
+# COMMAND ----------
+
+dbutils.notebook.run('../bronze/ingest_races_file',0,{'file_name':'races','container_name':'silver'})
+
+# COMMAND ----------
+
+dbutils.notebook.run('../bronze/ingest_results_file',0,{'file_name':'results','container_name':'silver'})
